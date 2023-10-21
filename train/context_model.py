a7e96c9e3017259cb01f16d2ee6fc1daaf0c6b4c
poirot0805 <826645693@qq.com>
2023-10-21 15:16:56 +0800

    preMask VERSION training debug

--- train/context_model.py
+++ train/context_model.py
@@ -142,7 +142,7 @@ def get_attention_mask(window_len, context_len, target_idx, device,
 
     # new:对角线下不被遮罩
     if ATTENTION_MODE=="PRE":
-        print(ATTENTION_MODE)
+        # print(ATTENTION_MODE)
         atten_mask.triu_(diagonal=1)
         atten_mask[:, target_idx] = False
         atten_mask[:, :context_len] = False
@@ -323,7 +323,11 @@ def train(config):
             # randomize transition length
             # trans_len = random.randint(min_trans, max_trans)# random.choice([random.randint(min_trans, max_trans),min(frame_nums)-2])
             max_trans_ = random.choice([max_trans,min(frame_nums)-2])
-            trans_len = random.randint(min_trans, max_trans_)
+            trans_len = max_trans_
+            if min_trans>= max_trans_:
+                trans_len = max_trans_
+            else:
+                trans_len = random.randint(min_trans, max_trans_)
             target_idx = context_len + trans_len
             seq_slice = slice(context_len, target_idx)
 
@@ -595,7 +599,7 @@ def eval_on_dataset(config, data_loader, model, trans_len,
             rotations=torch.cat([torch.zeros([rotations.shape[0],SEQNUM_GEO,*rotations.shape[2:]],dtype=dtype,device=device),rotations],dim=1) # GEO
         
         (gpos_batch_loss, gquat_batch_loss,
-         npss_batch_loss, npss_batch_weights) = \
+         npss_batch_loss, npss_batch_weights,_,_) = \
             benchmark.get_rmi_style_batch_loss(
                 positions, rotations, pos_new, rot_new, None,
                 context_len, target_idx, mean_rmi, std_rmi
